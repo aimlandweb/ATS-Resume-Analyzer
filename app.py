6f54d8659b1d5e5e214da732a3c50525c72edc87
aimlandweb <50330315+aimlandweb@users.noreply.github.com>
2024-01-24 14:52:34 -0800

    Updated App
     - Added Sections
     - Updated Prompts

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import io
 from PIL import Image
 import pdf2image
 import google.generativeai as genai
+from prompts import input_prompt1, input_prompt2, input_prompt3, input_prompt4, input_prompt5, input_prompt6, input_prompt7,input_prompt8
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
@@ -64,11 +65,24 @@ def input_pdf_setup(uploaded_file):
 
 st.set_page_config(page_title="ATS Resume Expert")
 st.header("ATS Tracking System")
+input_name = st.text_input("Enter your name: ", key="name", placeholder="John Doe")
+input_role = st.text_input(
+    "Role you are applying for: ", key="role", placeholder="Software Engineer"
+)
 input_text = st.text_area(
     "Job Description: ", key="input", placeholder="Paste the job description here..."
 )
 uploaded_file = st.file_uploader("Upload your resume(PDF)", type=["pdf"])
 
+
+if "input_text" not in st.session_state:
+    st.session_state["input_text"] = input_text
+
+if "input_name" not in st.session_state:
+    st.session_state["input_name"] = input_name
+
+if "input_role" not in st.session_state:
+    st.session_state["input_role"] = input_role
 
 if "input_text" not in st.session_state:
     st.session_state["input_text"] = input_text
@@ -80,27 +94,19 @@ if "uploaded_file" not in st.session_state:
 if uploaded_file is not None:
     st.write("PDF uploaded successfully!")
 
-input_prompt1 = """As an HR professional with experience in [User-Specified Field], analyze the submitted resume against the provided job description. Offer a detailed evaluation of the candidate's suitability for the role in [User-Specified Field], highlighting key strengths and areas for development. Format the response in markdown, including a table for skills and experiences pertinent to the job.
-                 """
-
-input_prompt2 = """Using your HR background in [User-Specified Field], assess the resume in relation to the job description. Provide feedback on the candidate's qualifications and recommend areas for skill enhancement relevant to [User-Specified Field]. Present your advice in markdown format. 
-                 """
-input_prompt3 = """Function as an ATS scanner with expertise in [User-Specified Field], and evaluate how well the resume aligns with the job description. Offer a percentage match, followed by a detailed markdown analysis. Include a two-column table for 'Keywords Used' and 'Keywords Missing', relevant to [User-Specified Field]."""
-
-input_prompt4 = """Revamp the candidate's resume to align it with the job description in [User-Specified Field]. Focus on presenting the candidate's strengths and suitability for the role clearly and professionally in markdown format."""
-
-input_prompt5 = """Revise and format the candidate's resume, ensuring it aligns effectively with the job description. Focus on clarity, relevance, and professional presentation. Structure the resume in a way that highlights the candidate's strengths and suitability for the role, using a markdown format for easy readability and a polished look."""
-
+    # Prompts here
 
 field = extract_field_from_description(input_text)
+print(field)
 
 dynamic_prompt1 = construct_prompt(input_prompt1, field)
 dynamic_prompt2 = construct_prompt(input_prompt2, field)
 dynamic_prompt3 = construct_prompt(input_prompt3, field)
 dynamic_prompt4 = construct_prompt(input_prompt4, field)
 dynamic_prompt5 = construct_prompt(input_prompt5, field)
-
-
+dynamic_prompt6 = construct_prompt(input_prompt6, field)
+dynamic_prompt7 = construct_prompt(input_prompt7, field)
+dynamic_prompt8 = construct_prompt(input_prompt8, field)
 # Check if both the job description and the PDF file are provided
 if input_text and uploaded_file:
     try:
@@ -114,18 +120,21 @@ if input_text and uploaded_file:
         # Process for each tab if PDF is valid
         field = extract_field_from_description(input_text)
 
-        tab1, tab2, tab3, tab4, tab5 = st.tabs(
+        tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs(
             [
-                "Analyze Resume",
-                "Skill Recommendations",
-                "Analysis",
+                "HR/Human Resume Analysis",
+                "ATS/AI Resume Analysis",
+                "Skills Review",
+                "ATS Friendly Resume",
                 "Cover Letter",
-                "Resume Format",
+                "LinkedIn",
+                "Interview Questions",
+                "Similar Companies",
             ]
         )
 
         with tab1:
-            st.header("Analyze Resume Fit for the Job")
+            st.header("HR/Human - Detailed Resume Analysis")
             if uploaded_file is not None:
                 # pdf_content = input_pdf_setup(uploaded_file)
                 response = get_gemini_response(dynamic_prompt1, pdf_content, input_text)
@@ -134,7 +143,7 @@ if input_text and uploaded_file:
                 st.write("Please upload the resume")
 
         with tab2:
-            st.header("Skill Enhancement Recommendations")
+            st.header("ATS/AI - Detailed Resume Analysis")
             if uploaded_file is not None:
                 # pdf_content = input_pdf_setup(uploaded_file)
                 response = get_gemini_response(dynamic_prompt2, pdf_content, input_text)
@@ -143,7 +152,7 @@ if input_text and uploaded_file:
                 st.write("Please upload the resume")
 
         with tab3:
-            st.header("Job Description and Resume Match Analysis")
+            st.header("Skills Recommended for the Role")
             if uploaded_file is not None:
                 # pdf_content = input_pdf_setup(uploaded_file)
                 response = get_gemini_response(dynamic_prompt3, pdf_content, input_text)
@@ -151,7 +160,7 @@ if input_text and uploaded_file:
             else:
                 st.write("Please upload the resume")
         with tab4:
-            st.header("Generate Tailored Cover Letter")
+            st.header("ATS Friendly and optimized Resume")
             if uploaded_file is not None:
                 # pdf_content = input_pdf_setup(uploaded_file)
                 response = get_gemini_response(dynamic_prompt4, pdf_content, input_text)
@@ -159,10 +168,34 @@ if input_text and uploaded_file:
             else:
                 st.write("Please upload the resume")
         with tab5:
-            st.header("Create an Updated Resume Format")
+            st.header("Cover Letter for your resume")
             if uploaded_file is not None:
                 # pdf_content = input_pdf_setup(uploaded_file)
                 response = get_gemini_response(dynamic_prompt5, pdf_content, input_text)
+                st.write(response)
+            else:
+                st.write("Please upload the resume")
+        with tab6:
+            st.header("LinkedIn Profile Recommendations")
+            if uploaded_file is not None:
+                # pdf_content = input_pdf_setup(uploaded_file)
+                response = get_gemini_response(dynamic_prompt6, pdf_content, input_text)
+                st.write(response)
+            else:
+                st.write("Please upload the resume")
+        with tab7:
+            st.header("Interview Questions and Questions to ask the interviewer")
+            if uploaded_file is not None:
+                # pdf_content = input_pdf_setup(uploaded_file)
+                response = get_gemini_response(dynamic_prompt7, pdf_content, input_text)
+                st.write(response)
+            else:
+                st.write("Please upload the resume")
+        with tab8:
+            st.header("Similar companies you can apply for")
+            if uploaded_file is not None:
+                # pdf_content = input_pdf_setup(uploaded_file)
+                response = get_gemini_response(dynamic_prompt8, pdf_content, input_text)
                 st.write(response)
             else:
                 st.write("Please upload the resume")
